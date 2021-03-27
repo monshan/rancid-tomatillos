e90d3bb70e25809ec2d6455db903e228fd7dda7f
Marika <marikashanahan@gmail.com>
2021-03-27 09:36:53 -0600

    Create ReturnButton component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import movieData from './movieData';
 import Theatre from './components/Theatre/Theatre';
 import SinglePoster from './components/SinglePoster/SinglePoster';
+import ReturnButton from './components/ReturnButton/ReturnButton'
 import { Component } from 'react';
 
 class App extends Component {
@@ -18,26 +19,36 @@ class App extends Component {
     console.log(id)
   }
 
+  deselect = () => {
+    this.setState({ movieId: 0 })
+  }
+
+  determineRetBtn = () => {
+
+  }
+
   render () {
+    let { selected } = this.state.movieId;
+
     return (
       <div className="App">
         <header className="header">
           <h1 className="title">Rancid Tomatillos</h1>
         </header>
         <main>
-        <SinglePoster posterClick={this.posterClick}/>
-          {/* { this.state.movieId &&
+          { this.state.movieId &&
             <SinglePoster posterClick={this.posterClick}/>
           }
           { !this.state.movieId &&
             <Theatre movies={this.state.movies} posterClick={this.posterClick}/>
-          } */}
+          }
         </main>
         <nav className="bottom-nav">
           <h2>Controlled Form</h2>
-          {/* { this.state.movieId &&
-            <ReturnButton />
+          {/* { selected &&
+            <ReturnButton deselect={ this.deselect }/>
           } */}
+          <ReturnButton />
         </nav>
       </div>
     )
---
+++ src/components/ReturnButton/ReturnButton.js
@@ -0,0 +1,22 @@
+// import { Component } from 'react'
+import './ReturnButton.css'
+
+// class ReturnButton extends Component {
+//   constructor(props) {
+//     super();
+//   }
+
+//   render () {
+//     return (
+//       <button className="returnButton" >Go back!</button>
+//     )
+//   }
+// }
+
+const ReturnButton = (props) => {
+  return (
+    <button className="returnButton">Go back!</button>
+  )
+}
+
+export default ReturnButton
