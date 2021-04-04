d6273547d4792d048ffe3131444cad871dff2d49
Marika <marikashanahan@gmail.com>
2021-04-03 21:21:50 -0600

    Apply hover overlay

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,16 +1,13 @@
 import './Poster.css';
 import { Link } from "react-router-dom";
 
-const Poster = ({ id, poster_path, backdrop_path, title, average_rating, release_date }) => {
+const Poster = ({ id, poster_path, title, average_rating, release_date }) => {
   return (
-    <Link to={`/${id}`} className="poster" id={id}>
-      <img src={poster_path} alt="Movie poster" className="movie-poster" />
-      <div className="movie-info">
+    <Link to={`/${id}`} className="poster" id={id} style={{backgroundImage: `url(${poster_path})`}}>
+      <div className="overlay">
         <h2>{ title }</h2>
         <p>{ release_date }</p>
         <p>{ Math.round(average_rating) } / 10</p>
-      </div>
-      <div className='overlay'>
       </div>
     </Link>
   )
